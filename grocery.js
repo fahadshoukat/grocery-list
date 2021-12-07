86ef72b1e15a11771145363dd7df5df60397c1ce
fahadshoukat <fraza6970@gmail.com>
2021-12-07 21:56:52 +0500

    selectElementsInJavaScript

---
+++ grocery.js
@@ -0,0 +1,6 @@
+const alertMessage = document.querySelector(".message");
+const inputValue = document.getElementById("inputBox");
+const addBtn = document.querySelector(".add-btn");
+const itemList = document.querySelector(".item-container");
+const clearBtn = document.querySelector(".clear-btn");
+
