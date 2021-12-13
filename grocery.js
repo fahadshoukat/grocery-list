dd0e0afbb616ace363b3cddd2ca76c6883e44eb5
fahadshoukat <fraza6970@gmail.com>
2021-12-13 21:51:16 +0500

    removePreviousCode
    remove js previous code because I'm confuse and can't understand what to do next.
    And remove article and add ul list to show grocery list

--- grocery.js
+++ grocery.js
@@ -5,78 +5,7 @@ const ListItem = document.querySelector(".list-item");
 const clearBtn = document.querySelector(".clear-btn");
 const itemContainer = document.querySelector(".item-container");
 
-let editItem;
-let editFlag = false;
-let editId = "";
 
-addBtn.addEventListener("click", function (e) {
-
-    e.preventDefault();
-    let value = input.value;
-    const id = new Date().getTime().toString();
-
-    if (value && !editFlag) {
-
-        const element = document.createElement("article");
-        let attr = document.createAttribute("data-id");
-        attr.value = id;
-        element.setAttributeNode(attr);
-        element.innerHTML = `<p class="title">${value}</p>
-        <div class="btn-container">
-            <button class="edit-btn">
-                <i class="fas fa-edit"></i>
-            </button>
-            <button class="delete-btn">
-                <i class="fas fa-trash-alt"></i>
-            </button>
-        </div>`;
-        ListItem.appendChild(element);
-        displayMessage("Item saved to list", "green");
-        itemContainer.classList.add("show-container");
-
-        const editBtn = document.querySelector(".edit-btn");
-        editBtn.addEventListener('click', function () {
-
-        });
-
-        const deleteBtn = document.querySelector(".delete-btn");
-        deleteBtn.addEventListener('click', function (e) {
-
-            const element = e.currentTarget.parentElement.parentElement;
-            console.log(element);
-            ListItem.removeChild(element);
-
-            if (ListItem.children.length === 0) {
-                itemContainer.classList.remove("show-container");
-            }
-
-
-        });
-
-        setBackToDefault();
-    }
-    else if (!value && editFlag) {
-        console.log("Editing");
-    }
-    else {
-        displayMessage("Empty value", "red");
-    }
-});
-
-clearBtn.addEventListener("click", function () {
-
-    const allItems = document.querySelectorAll(".list-item");
-    console.log(allItems);
-    if (allItems.length > 0) {
-        allItems.forEach((item) => {
-            ListItem.removeChild(item);
-        });
-    }
-
-    itemContainer.classList.remove("show-container");
-    displayMessage("Empty List", "red");
-    setBackToDefault();
-})
 
 function displayMessage(text, color) {
     alertMessage.textContent = text;
@@ -90,7 +19,5 @@ function displayMessage(text, color) {
 
 function setBackToDefault() {
     input.value = "";
-    editFlag = false;
-    editId = "";
     addBtn.textContent = "Add";
 }
